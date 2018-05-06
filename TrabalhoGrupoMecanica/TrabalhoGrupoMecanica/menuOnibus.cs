ee8ad68fbd0a6e8a269418a55142441a711d7edb
EduardoValoisFranz <dudufranz13@gmail.com>
2018-05-06 13:50:48 -0300

    validacao menuOnibus

--- TrabalhoGrupoMecanica/TrabalhoGrupoMecanica/menuOnibus.cs
+++ TrabalhoGrupoMecanica/TrabalhoGrupoMecanica/menuOnibus.cs
@@ -17,25 +17,135 @@ namespace TrabalhoGrupoMecanica
 
     public void CadastrarOnibus()
     {
-        Console.Write("Digite o modelo do ônibus: ");
-        onibusModelo[atual] = Console.ReadLine();
+            bool validarTexto = true;
+            while (validarTexto == true)
+            {
+                try
+                {
+                    Console.Write("Digite o modelo do ônibus: ");
+                    onibusModelo[atual] = Console.ReadLine();
+                    if (onibusModelo[atual].Length >= 5)
+                    {
+                        validarTexto = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Modelo do ônibus deve conter no mínimo 5 caracteres, digite novamente");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Nome do ônibus invalido, digite novamente");
+                }
+            }
+            bool validarAno = true;
+            while (validarAno == true)
+            {
+                try
+                {
+                    Console.Write("Digite o ano do ônibus: ");
+                    onibusAno[atual] = Convert.ToInt32(Console.ReadLine());
+                    if (onibusAno[atual] > 0)
+                    {
+                        validarAno = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ano do ônibus invalido solicitado é 0 ou menor, digite novamente");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ano do ônibus invalido, digite novamente");
+                }
+            }
+            bool validarCor = true;
+            while (validarCor == true)
+            {
+                try
+                {
+                    Console.Write("Digite a cor do ônibus: ");
+                    onibusCor[atual] = Console.ReadLine();
+                    if (onibusCor[atual].Length >= 5)
+                    {
+                        validarCor = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cor do ônibus deve conter no mínimo 5 caracteres, digite novamente");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Cor do ônibus invalida, digite novamente");
+                }
+            }
 
-        Console.Write("Digite o ano de fabricação: ");
-        onibusAno[atual] = Convert.ToInt32(Console.ReadLine());
+            bool validarPreco = true;
+            while (validarPreco == true)
+            {
+                try
+                {
+                    Console.Write("Digite o preço do ônibus: ");
+                    onibusPreco[atual] = Convert.ToDouble(Console.ReadLine());
+                    if (onibusPreco[atual] > 0)
+                    {
+                        validarPreco = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Preço do ônibus solicitado é 0 ou menor, digite novamente");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Preço do ônibus invalido, digite novamente");
+                }
+            }
+            bool validarPeso = true;
+            while (validarPeso == true)
+            {
+                try
+                {
+                    Console.Write("Digite o peso da moto: ");
+                    onibusPeso[atual] = Convert.ToDouble(Console.ReadLine());
+                    if (onibusPeso[atual] > 0)
+                    {
+                        validarPeso = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Peso do ônibus solicitado é 0 ou menor, digite novamente");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Peso do ônibus invalido, digite novamente");
+                }
+            }
+            bool validarPassageiros = true;
+            while (validarPassageiros == true)
+            {
+                try
+                {
+                    Console.Write("Digite quantos passageiros comporta: ");
+                    onibusPessoas[atual] = Convert.ToInt32(Console.ReadLine());
+                    if (onibusPessoas[atual] > 0)
+                    {
+                        validarPassageiros = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Quantidade de passageiros solicitado é 0 ou menor, digite novamente");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Quantidade de passageiros invalida, digite novamente");
+                }
+            }
 
-        Console.Write("Digite a cor do ônibus: ");
-        onibusCor[atual] = Console.ReadLine();
-
-        Console.Write("Digite o preço do ônibus: ");
-        onibusPreco[atual] = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Digite o peso do ônibus: ");
-        onibusPeso[atual] = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Digite quantos passageiros comporta: ");
-        onibusPessoas[atual] = Convert.ToInt32(Console.ReadLine());
-
-        Console.Clear();
+            Console.Clear();
 
         atual++;
 
@@ -58,26 +168,136 @@ namespace TrabalhoGrupoMecanica
         if (posicao != -1)
         {
 
-            Console.Write("Digite o modelo do avião: ");
-            onibusModelo[posicao] = Console.ReadLine();
+                bool validarTexto = true;
+                while (validarTexto == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite o modelo do ônibus: ");
+                        onibusModelo[posicao] = Console.ReadLine();
+                        if (onibusModelo[posicao].Length >= 5)
+                        {
+                            validarTexto = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Modelo do ônibus deve conter no mínimo 5 caracteres, digite novamente");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Nome do ônibus invalido, digite novamente");
+                    }
+                }
+                bool validarAno = true;
+                while (validarAno == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite o ano do ônibus: ");
+                        onibusAno[posicao] = Convert.ToInt32(Console.ReadLine());
+                        if (onibusAno[posicao] > 0)
+                        {
+                            validarAno = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ano do ônibus invalido solicitado é 0 ou menor, digite novamente");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Ano do ônibus invalido, digite novamente");
+                    }
+                }
+                bool validarCor = true;
+                while (validarCor == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite a cor do ônibus: ");
+                        onibusCor[posicao] = Console.ReadLine();
+                        if (onibusCor[posicao].Length >= 5)
+                        {
+                            validarCor = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cor do ônibus deve conter no mínimo 5 caracteres, digite novamente");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Cor do ônibus invalida, digite novamente");
+                    }
+                }
 
-            Console.Write("Digite o ano de fabricação: ");
-            onibusAno[posicao] = Convert.ToInt32(Console.ReadLine());
+                bool validarPreco = true;
+                while (validarPreco == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite o preço do ônibus: ");
+                        onibusPreco[posicao] = Convert.ToDouble(Console.ReadLine());
+                        if (onibusPreco[posicao] > 0)
+                        {
+                            validarPreco = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Preço do ônibus solicitado é 0 ou menor, digite novamente");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Preço do ônibus invalido, digite novamente");
+                    }
+                }
+                bool validarPeso = true;
+                while (validarPeso == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite o peso da moto: ");
+                        onibusPeso[posicao] = Convert.ToDouble(Console.ReadLine());
+                        if (onibusPeso[posicao] > 0)
+                        {
+                            validarPeso = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Peso do ônibus solicitado é 0 ou menor, digite novamente");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Peso do ônibus invalido, digite novamente");
+                    }
+                }
+                bool validarPassageiros = true;
+                while (validarPassageiros == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite quantos passageiros comporta: ");
+                        onibusPessoas[posicao] = Convert.ToInt32(Console.ReadLine());
+                        if (onibusPessoas[posicao] > 0)
+                        {
+                            validarPassageiros = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Quantidade de passageiros solicitado é 0 ou menor, digite novamente");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Quantidade de passageiros invalida, digite novamente");
+                    }
+                }
 
-            Console.Write("Digite a cor do avião: ");
-            onibusCor[posicao] = Console.ReadLine();
 
-            Console.Write("Digite o preço do avião: ");
-            onibusPreco[posicao] = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite o peso do avião: ");
-            onibusPeso[posicao] = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite quantos passageiros comporta: ");
-            onibusPessoas[posicao] = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.Clear();
+                Console.Clear();
 
             Console.WriteLine("Dados alterados com sucesso");
         }
@@ -103,7 +323,7 @@ namespace TrabalhoGrupoMecanica
     public void BuscarOnibus()
     {
         Console.WriteLine("Digite o nome do ônibus para a busca: ");
-        string modeloBuscado = Console.ReadLine();
+        string modeloBuscado = Console.ReadLine().ToLower().Trim();
         bool encontrado = false;
         for (int i = 0; i < atual; i++)
         {
